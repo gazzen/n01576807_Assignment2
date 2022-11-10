f2aed3326208b731f48321e90b4d8d517bc273a0
gazzen <gazzen@users.noreply.github.com>
2022-11-09 19:00:44 -0500

    Update rollDiceController.cs
    edit commit

--- calorieCounter/calorieCounter/Controllers/rollDiceController.cs
+++ calorieCounter/calorieCounter/Controllers/rollDiceController.cs
@@ -9,13 +9,14 @@ using System.Xml.Schema;
 
 namespace calorieCounter.Controllers
 {
+    [Route("api/dice/{m}/{n}")]
     public class rollDiceController : ApiController
     {
         public int DiceGame( int m, int n)
         {
             m = 6;
             n = 6;
-            int total = 0;
+            int counter = 0;
             int fails = 0;
 
             
@@ -26,9 +27,9 @@ namespace calorieCounter.Controllers
                 for (int j = 1; j < n; j++)
                 {
                     if (m + n == 10)
-                        total = total + 1;
+                        counter++;
                     else
-                        fails = fails + 1;
+                        fails++;
                 }
                     
             }
